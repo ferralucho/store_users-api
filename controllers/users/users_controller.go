af5a2b8b929c7fedaee09f70fa363f8ebff54b63
Luciano Ferrari <luciano.ferrari@mercadolibre.com>
2020-03-31 19:11:19 -0300

    add error and create user

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,9 +1,27 @@
 package users
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mercadolibre/store_users-api/domain/users"
+	"github.com/mercadolibre/store_users-api/services"
+	"github.com/mercadolibre/store_users-api/utils/errors"
+	"net/http"
+)
 
-func CreateUser(c *gin.Context) {
+func Create(c *gin.Context) {
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
 
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"github.com/mercadolibre/store_users-api/domain/users"
+	"github.com/mercadolibre/store_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return nil, nil
+}
